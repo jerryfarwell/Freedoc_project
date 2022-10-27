5e55110cf00fcb42fd100610bebb46ec9693b2f6
JERRY farwell <114482666+jerryfarwell@users.noreply.github.com>
2022-10-27 22:15:03 +0200

    first commit

---
+++ app/models/speciality.rb
@@ -0,0 +1,5 @@
+class Speciality < ApplicationRecord
+    has_many :join_table_specialty_doctors
+    has_many :doctors, through: :join_table_specialty_doctors
+
+end
---
+++ db/migrate/20221027194717_create_jointablespecialitydoctors.rb
@@ -0,0 +1,8 @@
+class CreateJointablespecialitydoctors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :jointablespecialitydoctors do |t|
+
+      t.timestamps
+    end
+  end
+end
